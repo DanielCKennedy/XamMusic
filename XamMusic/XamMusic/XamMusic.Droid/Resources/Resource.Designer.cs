2515c188151cdaf2f5c40a9dfaea0e9343366e32
DanielCKennedy <danielckennedy@hotmail.com>
2017-05-23 18:03:38 -0700

    Music bar UI

--- XamMusic/XamMusic/XamMusic.Droid/Resources/Resource.Designer.cs
+++ XamMusic/XamMusic/XamMusic.Droid/Resources/Resource.Designer.cs
@@ -2200,8 +2200,20 @@ namespace XamMusic.Droid
 			// aapt resource value: 0x7f0200a0
 			public const int mr_ic_play_light = 2130837664;
 			
+			// aapt resource value: 0x7f0200a5
+			public const int notification_template_icon_bg = 2130837669;
+			
 			// aapt resource value: 0x7f0200a1
-			public const int notification_template_icon_bg = 2130837665;
+			public const int pause = 2130837665;
+			
+			// aapt resource value: 0x7f0200a2
+			public const int pause_w = 2130837666;
+			
+			// aapt resource value: 0x7f0200a3
+			public const int play = 2130837667;
+			
+			// aapt resource value: 0x7f0200a4
+			public const int play_w = 2130837668;
 			
 			static Drawable()
 			{
---
+++ XamMusic/XamMusic/XamMusic/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamMusic.Controls;
+using XamMusic.ViewModels;
+using XamMusic.Views;
+
+namespace XamMusic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // initialize MusicStateViewModel to load current playlist as app loads
+            var a = MusicStateViewModel.Instance;
+            var b = QueuePopup.Instance;
+            var c = SliderControl.Instance;
+
+            MainPage = new RootPage();
+        }
+    }
+}
