a70d928be02ebdfb1bb0bd4c26ab7b1f5805c058
DanielCKennedy <danielckennedy@hotmail.com>
2017-05-25 19:12:37 -0700

    Menu UI

--- XamMusic/XamMusic/XamMusic/Models/PlaylistItem.cs
+++ XamMusic/XamMusic/XamMusic/Models/PlaylistItem.cs
@@ -9,11 +9,34 @@ using XamMusic.ViewModels;
 
 namespace XamMusic.Models
 {
-    public class PlaylistItem
+    public class PlaylistItem : BaseViewModel
     {
         public Playlist Playlist { get; set; }
 
         public Command AddSong { get; set; }
+
+        private bool _isToggled;
+        public bool IsToggled
+        {
+            get
+            {
+                return _isToggled;
+            }
+            set
+            {
+                _isToggled = value;
+                OnPropertyChanged(nameof(CanAdd));
+            }
+        }
+
+        public bool CanAdd
+        {
+            get
+            {
+                return Playlist.IsDynamic && _isToggled;
+            }
+        }
+
 
         public PlaylistItem(Playlist playlist)
         {
@@ -40,5 +63,7 @@ namespace XamMusic.Models
                 });
             }
         }
+
+        
     }
 }
--- XamMusic/XamMusic/XamMusic/Views/MenuPage.xaml.cs
+++ XamMusic/XamMusic/XamMusic/Views/MenuPage.xaml.cs
@@ -9,6 +9,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamMusic.ViewModels;
 using XamMusic.Controls;
+using XamMusic.Models;
 
 namespace XamMusic.Views
 {
@@ -26,6 +27,20 @@ namespace XamMusic.Views
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushPopupAsync(new CreatePlaylistPopup(), true);
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            ((Image)sender).Opacity = 0.6;
+            ((Image)sender).FadeTo(1, 150);
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            foreach (PlaylistItem item in MenuViewModel.Instance.PlaylistItems)
+            {
+                item.IsToggled = ((Switch)sender).IsToggled;
+            }
         }
     }
 }
